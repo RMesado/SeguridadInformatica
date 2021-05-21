77b1b06c6bea3977d6e08a51d7f5def6f9120bce
RMesado <rafaelmesado@gmail.com>
2021-05-22 01:30:24 +0200

    mas bonico aun

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ include(dirname(__FILE__) . "/includes/login_usuario.php");
     <title>FileSafety</title>
     <script src="funciones.js"></script>
 
+    <link rel="shortcut icon" type="image/jpg" href="images/descarga.jfif"/>
     <!-- Include Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="estilo.css" type="text/css">
@@ -25,7 +26,7 @@ include(dirname(__FILE__) . "/includes/login_usuario.php");
 include_once "includes/header.php";
 ?>
 <main class="flex-shrink-0">
-    <div class="container p-2">
+    <div class="container">
         <?php
         if (!isset($_SESSION['u_id'])) {
             ?>
@@ -49,74 +50,105 @@ include_once "includes/footer.php";
 ?>
 
 <!-- LOGIN Modal -->
-<div id="login" class="modal">
+<div id="login" class="modal" tabindex="-1" role="dialog" aria-labelledby="titleModal" aria-hidden="true">
     <!-- Modal content -->
-    <div class="modal-content">
-        <header class="modal-header">
-            <h5 class="modal-header">Datos de inicio de sesión</h5>
-            <button aria-label="Close" class="close">&times;</button>
-        </header>
-        <form class="form_usuario" action="includes/loginUsuario-intermedio.php" method="post">
-            <label>Usuario</label>
-            <br/>
-            <label>
-                <input type="text" id="login_username" name="user" required/>
-            </label>
-            <br/>
-            <br/>
-            <label>Contraseña</label>
-            <br/>
-            <label>
-                <input type="password" id="login_password" name="password" required/>
-            </label>
-            <br/>
-            <br/>
-            <button type="submit" name="loginsubmit">Iniciar sesión</button>
-            <p>¿Aún no tienes cuenta?
-                <a href="javascript:changeModal()">Regístratre</a>
-            </p>
-        </form>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="titleModal">Datos de registro</h5>
+                <button aria-label="Close" class="close">&times;</button>
+            </div>
+            <div class="modal-body">
+                <div class="justify-content-center">
+                    <div>
+                        <div class="card-body">
+                            <div class="tab-content">
+                                <div class="tab-pane fade show active" role="tabpanel">
+                                    <form class="form-group" action="includes/loginUsuario-intermedio.php" method="post">
+                                        <div class="form-group">
+                                            <h4>Usuario</h4>
+                                            <input type="text" class="form-control" id="reg_username" name="user"
+                                                   placeholder="Introduce usuario o email" required/>
+                                        </div>
+                                        <div class="form-group">
+                                            <h4>Contraseña</h4>
+                                            <input class="form-control" type="password" id="reg_password"
+                                                   name="password"
+                                                   placeholder="Introduce la contraseña" required/>
+                                        </div>
+                                        <button class="btn btn-dark btn-block" type="submit" name="loginsubmit">Iniciar
+                                            sesión
+                                        </button>
+                                        <div class="form-group">
+                                            <p>¿Aún no tienes cuenta?
+                                                <a href="javascript:changeModal()">Regístratre</a>
+                                            </p>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
 <!--Register modal-->
-<div id="register" class="modal">
+<div id="register" class="modal" tabindex="-1" role="dialog" aria-labelledby="titleModal" aria-hidden="true">
     <!-- Modal content -->
-    <div class="modal-content">
-        <header class="modal-header">
-            <h5>Datos de registro</h5>
-            <button aria-label="Close" class="close">&times;</button>
-        </header>
-        <form class="form-group" action="includes/regUsuario-intermedio.php" method="post">
-            <div class="form-group">
-                <label for="reg_username">Usuario</label>
-                <input type="text" class="form-control" id="reg_username" name="username"
-                       placeholder="Introduce un usuario" required/>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="titleModal">Datos de registro</h5>
+                <button aria-label="Close" class="close">&times;</button>
             </div>
-            <div class="form-group">
-                <label for="email">Correo</label>
-                <input class="form-control" type="email" id="email" name="email" placeholder="Introduce un email"
-                       required/>
+            <div class="modal-body">
+                <div class="justify-content-center">
+                    <div>
+                        <div class="card-body">
+                            <div class="tab-content">
+                                <div class="tab-pane fade show active" role="tabpanel">
+                                    <form class="form-group" action="includes/regUsuario-intermedio.php" method="post">
+                                        <div class="form-group">
+                                            <h4>Usuario</h4>
+                                            <input type="text" class="form-control" id="reg_username" name="username"
+                                                   placeholder="Introduce un usuario" required/>
+                                        </div>
+                                        <div class="form-group">
+                                            <h4>Correo</h4>
+                                            <input class="form-control" type="email" id="email" name="email"
+                                                   placeholder="Introduce un email"
+                                                   required/>
+                                        </div>
+                                        <div class="form-group">
+                                            <h4>Contraseña</h4>
+                                            <input class="form-control" type="password" id="reg_password"
+                                                   name="password"
+                                                   placeholder="Introduce una contraseña" required/>
+                                        </div>
+                                        <div class="form-group">
+                                            <h4>Confirma la contraseña</h4>
+                                            <input class="form-control" type="password" id="password2" name="password2"
+                                                   placeholder="Confirma la contraseña" required/>
+                                        </div>
+                                        <button class="btn btn-dark btn-block" type="submit" name="registrosubmit">
+                                            Registrarse
+                                        </button>
+                                        <div class="form-group">
+                                            <p>¿Ya tienes cuenta?
+                                                <a href="javascript:changeModal()">Inicia sesión</a>
+                                            </p>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="reg_password">Contraseña</label>
-                <input class="form-control" type="password" id="reg_password" name="password"
-                       placeholder="Introduce una contraseña" required/>
-            </div>
-            <div class="form-group">
-                <label for="password2">Confirma la contraseña</label>
-                <input class="form-control" type="password" id="password2" name="password2"
-                       placeholder="Confirma la contraseña" required/>
-            </div>
-            <div class="form-group">
-                <button class="btn btn-dark" type="submit" name="registrosubmit">Registrarse</button>
-            </div>
-            <div class="form-group">
-                <p>¿Ya tienes cuenta?
-                    <a href="javascript:changeModal()">Inicia sesión</a>
-                </p>
-            </div>
-        </form>
+        </div>
     </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
@@ -130,7 +162,6 @@ include_once "includes/footer.php";
     // Get the button that opens the modal
     var loginbtn = document.getElementById("loginbtn");
     var registerbtn = document.getElementById("registerbtn");
-    var logoutbtn = document.getElementById("logoutbtn");
 
     // Get the <span> element that closes the modal
     var span_login = document.getElementsByClassName("close")[0];
@@ -145,15 +176,6 @@ include_once "includes/footer.php";
     if (registerbtn != null) {
         registerbtn.onclick = function () {
             modal_register.style.display = "block";
-        }
-    }
-    if (logoutbtn != null) {
-        logoutbtn.onclick = function () {
-            <?php
-            unset($_SESSION['u_id']);
-            $_SESSION['username'] = '';
-            ?>
-            location.reload()
         }
     }
 
