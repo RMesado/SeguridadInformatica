29b0ddb74bcd509263017461a9959eeb8f8a322e
rmesado <rafaelmesado@gmail.com>
2021-05-24 00:35:44 +0200

    footer dinamico

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="navbar fixed-bottom bg-dark text-center text-lg-start">
     <!-- Copyright -->
     <div class="mx-auto order-0">
-    <a style="text-decoration: none; color: white;">
+    <a class="tam" style="text-decoration: none; color: white;">
         © 2021 Copyright: Rafael Mesado &amp; Raquel Garvi
     </a>
     </div>
